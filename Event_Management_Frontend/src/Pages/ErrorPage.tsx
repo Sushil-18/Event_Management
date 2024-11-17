c867576249c1c95ad3a05b1c421ee95c27dc9a9b
sushil-18 <sushilkumarwaghmare9@gmail.com>
2024-11-17 22:28:56 +0530

    Implemented the error page inspired by Netflix🤩

--- Event_Management_Frontend/src/Pages/ErrorPage.tsx
+++ Event_Management_Frontend/src/Pages/ErrorPage.tsx
@@ -1,9 +1,34 @@
-import React from "react";
+import { useNavigate } from "react-router";
+import LostVechie from "../assets/Lost-in-space.jpg";
 
-const ErrorPage = () => {
+const ErrorPage: React.FC = () => {
+  const navigate = useNavigate();
+
+  function handleClick(): void {
+    navigate("/");
+  }
+
   return (
-    <div>
-      <p>This is the error page</p>
+    <div
+      className="flex flex-col items-center min-h-screen bg-cover bg-center"
+      style={{ backgroundImage: `url(${LostVechie})` }}
+    >
+      <h1 className="text-5xl mt-48 font-bold text-shadow-md">
+        Lost Your Way?
+      </h1>
+      <p className="text-gray-100 mt-8 text-2xl">
+        Sorry, We can't find that page. You'll find lots to explore on the Home
+        Page
+      </p>
+      <button
+        onClick={handleClick}
+        className="mt-8 px-4 py-2 bg-white text-black rounded hover:bg-gray-200 mb-8"
+      >
+        HomePage
+      </button>
+      <p className="mt-4 text-2xl border-l-2 border-red-800 p-2">
+        Error Code <span className="text-white">HTTP-404</span>
+      </p>
     </div>
   );
 };
