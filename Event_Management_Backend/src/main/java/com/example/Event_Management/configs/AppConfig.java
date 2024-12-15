e60dc174f5e5316321405504587b8536d62b5cd7
sushil-18 <sushilkumarwaghmare9@gmail.com>
2024-12-15 22:26:18 +0530

    Added cors config in backend

--- Event_Management_Backend/src/main/java/com/example/Event_Management/configs/AppConfig.java
+++ Event_Management_Backend/src/main/java/com/example/Event_Management/configs/AppConfig.java
@@ -3,11 +3,27 @@ package com.example.Event_Management.configs;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class AppConfig {
     @Bean
     ModelMapper getModelMapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        }
     }
 }
